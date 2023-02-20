367eab6240577ef821964bc17251012f01be165b
qizhang <13681466643@163.com>
2023-02-21 00:43:20 +0800

    feat:blob

---
+++ .bin/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+pnpm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git remote add origin git@github.com:Pancake-Q/node-web.git
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Pancake-Q/node-web.git main
+
+cd -
